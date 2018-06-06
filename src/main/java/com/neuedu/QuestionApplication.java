0d8dee776209d8630f48271b74603bfd25e21e18
guoyang <1035610621@qq.com>
2018-06-06 09:02:45 +0800

    设置字符编码

--- src/main/java/com/neuedu/QuestionApplication.java
+++ src/main/java/com/neuedu/QuestionApplication.java
@@ -2,6 +2,9 @@ package com.neuedu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.CharacterEncodingFilter;
 
 @SpringBootApplication
 public class QuestionApplication {
@@ -9,4 +12,15 @@ public class QuestionApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(QuestionApplication.class, args);
 	}
+
+	@Bean
+	public FilterRegistrationBean filterRegistrationBean() {
+		FilterRegistrationBean registrationBean = new FilterRegistrationBean();
+		CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+		characterEncodingFilter.setForceEncoding(true);
+		characterEncodingFilter.setEncoding("UTF-8");
+		registrationBean.setFilter(characterEncodingFilter);
+		return registrationBean;
+	}
+
 }
